d287a9288658c0552bc8c6739489575219137a5e
stouvoli <stouvoli@gmail.com>
2017-04-17 11:59:01 +0000

    Initial old application

---
+++ app/src/main/java/com/numeris_ci/simpleflashlight/MainActivity.java
@@ -0,0 +1,121 @@
+package com.numeris_ci.simpleflashlight;
+
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+    private android.hardware.Camera camera;
+    private boolean isFlashOn;
+    private boolean hasFlash;
+    android.hardware.Camera.Parameters param;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //check if flash light is supported
+        hasFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash) {
+            //flash not supported
+            //show alert message and close app
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            //Add button Ok
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            builder.setTitle("Your device doesn't support flash light!");
+            //Create the AlertDialog
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        getCamera();
+        ToggleButton flashSwitch = (ToggleButton) findViewById(R.id.flash_switch);
+        flashSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    turnOnFlash();
+                } else {
+                    turnOffFlash();
+                }
+            }
+        });
+
+    }
+
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = android.hardware.Camera.open();
+                param = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("SimpleFlashLight", "Camera Error: " + e.getMessage());
+
+            }
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        turnOffFlash();
+    }
+
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || param == null) {
+                return;
+            }
+            param = camera.getParameters();
+            param.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(param);
+            camera.startPreview();
+            isFlashOn = true;
+
+            Log.v("SimpleFlashLight", "Flash has been turned on ...");
+        }
+    }
+
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || param == null) {
+                return;
+            }
+            param = camera.getParameters();
+            param.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(param);
+            camera.stopPreview();
+            isFlashOn = false;
+
+            Log.v("SimpleFlashLight", "Flash has been turned off ...");
+        }
+    }
+}
